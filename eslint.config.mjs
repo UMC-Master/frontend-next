c141b2959ffd837fc3d052b9e4fee40a03800800
Minji Kim <minjikim2000@hotmail.com>
2025-09-15 23:46:05 +0900

    🔧 config: eslint & prettier 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import prettierPlugin from "eslint-plugin-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,15 +11,30 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals"),
+  ...compat.extends("plugin:prettier/recommended"),
+
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "no-undef": "error",
+      "prettier/prettier": [
+        "error",
+        {
+          singleQuote: true,
+          semi: true,
+          tabWidth: 2,
+          trailingComma: "all",
+          printWidth: 80,
+          bracketSpacing: true,
+          arrowParens: "avoid",
+          endOfLine: "lf",
+        },
+      ],
+    },
   },
 ];
 
