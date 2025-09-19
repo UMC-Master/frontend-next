d2aa3523e20caea0c093a31eb11d4d510f966062
Minji Kim <minjikim2000@hotmail.com>
2025-09-19 19:38:21 +0900

    🩹 chore: pretendard 폰트 추가 #1

---
+++ src/lib/fonts/pretendard.ts
@@ -0,0 +1,14 @@
+import localFont from 'next/font/local';
+
+export const pretendard = localFont({
+  src: [
+    {
+      path: '../../assets/fonts/PretendardVariable.woff2',
+      weight: '100 900',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-pretendard',
+  display: 'swap',
+  preload: true,
+});
