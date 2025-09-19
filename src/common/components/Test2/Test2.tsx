69c04b8574c42eeab8db866d292583f1603eee6c
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-09-19 16:18:06 +0900

    ✅ Test: Test

--- src/common/components/Test2/Test2.tsx
+++
@@ -1,9 +0,0 @@
-const Test2 = () => {
-  return (
-    <div className="p-4 border border-gray-300 rounded">
-      <button>Test Component</button>
-    </div>
-  );
-};
-
-export default Test2;
---
+++ src/common/components/Test3/Test3.tsx
@@ -0,0 +1,11 @@
+const Test3 = () => {
+  return (
+    <div className="p-4 border border-gray-300 rounded">
+      <button className="p-2 bg-blue-500 text-white rounded">
+        Test Component
+      </button>
+    </div>
+  );
+};
+
+export default Test3;
