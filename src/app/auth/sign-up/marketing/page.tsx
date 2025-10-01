26e3e9acf323a2e00ddc14ed20c4c1ee3f80b2b2
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-10-01 22:05:30 +0900

    ✨ Feat: TermStep 구현 ( 회원가입 Step 1 )

---
+++ src/app/auth/sign-up/marketing/page.tsx
@@ -0,0 +1,11 @@
+import DetailMarketing from '@/features/auth/sign-up/components/TermDetail/DetailMarketing';
+
+const PrivacyPage = () => {
+  return (
+    <div>
+      <DetailMarketing />
+    </div>
+  );
+};
+
+export default PrivacyPage;
---
+++ src/app/auth/sign-up/term/page.tsx
@@ -0,0 +1,11 @@
+import DetailTerm from '@/features/auth/sign-up/components/TermDetail/DetailTerm';
+
+const TermPage = () => {
+  return (
+    <div>
+      <DetailTerm />
+    </div>
+  );
+};
+
+export default TermPage;
