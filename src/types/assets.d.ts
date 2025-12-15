17beb1fb802a834f6901533d4d1d4ff6c3ae0c8c
Minji Kim <minjikim2000@hotmail.com>
2025-12-16 00:16:02 +0900

    🔧 config: 이미지 임포트 설정 추가

---
+++ src/types/assets.d.ts
@@ -0,0 +1,26 @@
+// src/types/assets.d.ts
+
+declare module '*.png' {
+  const src: import('next/image').StaticImageData;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: import('next/image').StaticImageData;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: import('next/image').StaticImageData;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: import('next/image').StaticImageData;
+  export default src;
+}
+
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
