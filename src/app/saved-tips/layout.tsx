ccf42f7b58d2870bab903dca147c5a9e1f236e07
Minji Kim <minjikim2000@hotmail.com>
2026-01-22 22:30:47 +0900

    ✨feat: add SavedTips page #16

---
+++ src/app/saved-tips/layout.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import BackIcon from '@/assets/svgs/arrow_backward.svg';
+import { useRouter } from 'next/navigation';
+import TitleHeader from '@/features/main/components/headers/TitleHeaderLayout';
+
+export default function SavedTipsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const router = useRouter();
+  return (
+    <>
+      <TitleHeader
+        title="저장한 꿀팁"
+        leftIcon={BackIcon}
+        onIconClick={() => router.back()}
+        sticky
+      />
+      <main className="">{children}</main>
+    </>
+  );
+}
